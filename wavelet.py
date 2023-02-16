85fe18d91db35d35db747d575f5f9f99888162a6
longwarriors <longwarriors@gmail.com>
2023-02-17 01:02:27 +0800

    homework

--- wavelet.py
+++ wavelet.py
@@ -1,4 +1,14 @@
-import torch 
+import torch
+
 
 class GaussianPacket:
-    """1d: https://zhuanlan.zhihu.com/p/343847938"""
+    """
+    1d: https://zhuanlan.zhihu.com/p/343847938
+    """
+
+    def __init__(self) -> None:
+        pass
+
+
+
+if __name__ == "__main__":
