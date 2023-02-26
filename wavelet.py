ea81859f9ea67e6ac34d68c5bf55288d1b601826
longwarriors <longwarriors@gmail.com>
2023-02-27 00:51:51 +0800

    format

--- wavelet.py
+++ wavelet.py
@@ -5,8 +5,10 @@ def Gaussian_Wave_Packet(k, sigma, miu, x):
     # k = torch.tensor([k])
     # sigma = torch.tensor([sigma])
     # miu = torch.tensor([miu])
-    envelope = torch.exp(-(x-miu)**2 / (2*sigma**2))
-    oscillate = torch.exp(1j*k*(x-miu)) # planner wave traveling in the positive x direction
+    envelope = torch.exp(-((x - miu) ** 2) / (2 * sigma**2))
+    oscillate = torch.exp(
+        1j * k * (x - miu)
+    )  # planner wave traveling in the positive x direction
     wavelet = envelope * oscillate
     return wavelet
 
@@ -14,6 +16,7 @@ def Gaussian_Wave_Packet(k, sigma, miu, x):
 class GaussianPacket:
     """
     1d: https://zhuanlan.zhihu.com/p/343847938
+        https://zhuanlan.zhihu.com/p/608469189
     """
 
     def __init__(self) -> None:
@@ -24,8 +27,8 @@ class GaussianPacket:
         sigma = torch.tensor([sigma])
         miu = torch.tensor([miu])
 
-        wave = torch.exp(-(x-miu)**2 / (2*sigma**2)) * torch.exp(1j*k*x)
-        wave = wave / wave.norm(p=1) # L1范数 = wave.abs().sum()
+        wave = torch.exp(-((x - miu) ** 2) / (2 * sigma**2)) * torch.exp(1j * k * x)
+        wave = wave / wave.norm(p=1)  # L1范数 = wave.abs().sum()
         return wave
 
 
